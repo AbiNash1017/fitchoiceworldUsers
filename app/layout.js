a6f46120e5be7504b41e9f197770d51fe7756553
Abinash Das <abinash9731@gmail.com>
2026-03-12 13:42:02 +0530

    prefix

--- app/layout.js
+++ app/layout.js
@@ -13,7 +13,7 @@ const geistMono = Geist_Mono({
 
 export const metadata = {
    title: "Fykli - Fit Choice World",
-    description: "Fykli - By Fit Choice World, Fitness based on your mood",
+    description: "Fykli By Fit Choice World, Fitness based on your mood",
 };
 
 export default function RootLayout({ children }) {
