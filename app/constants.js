420883e1eabfa2a46ad5a60594e5459d1534f208
Abinash Das <abinash9731@gmail.com>
2026-03-26 15:24:00 +0530

    updated constants

--- app/constants.js
+++ app/constants.js
@@ -1,31 +1,57 @@
 export const LOGO_URL = "/images/fcw_transparent.png";
 
 export const SOCIAL_LINKS = {
-    instagram: "#",
-    twitter: "#",
-    facebook: "#",
-    linkedin: "#"
+  instagram: "#",
+  twitter: "#",
+  facebook: "#",
+  linkedin: "#"
 };
 
 export const APP_LINKS = {
-    apple: "https://apps.apple.com/in/app/fit-choice-world/id6745477875",
-    google: "https://play.google.com/store/apps/details?id=com.fcw.fitchoice_world_app"
+  apple: "https://apps.apple.com/in/app/fit-choice-world/id6745477875",
+  google: "https://play.google.com/store/apps/details?id=com.fcw.fitchoice_world_app"
 };
 
 export const CONTACT_INFO = {
-    email: "support@fitchoiceworld.com",
-    phone: "+91 9036815005",
-    address: "1st Floor, 911, 28th Main Rd, above Karnataka Bank, Putlanpalya, Jayanagara 9th Block, Jayanagar, Bengaluru, Karnataka 560041"
+  email: "support@fitchoiceworld.com",
+  phone: "+91 9036815005",
+  address: "1st Floor, 911, 28th Main Rd, above Karnataka Bank, Putlanpalya, Jayanagara 9th Block, Jayanagar, Bengaluru, Karnataka 560041"
 };
 
 export const FITNESS_FACILITIES = [
-  "GYM", "YOGA", "ZUMBA", "PERSONAL TRAINING", "SWIMMING", "BOXING", 
-  "TAEKWONDO", "POWERLIFTING", "CROSS FIT"
+  "GYM",
+  "YOGA",
+  "ZUMBA",
+  "PERSONAL TRAINING",
+  "SWIMMING",
+  "BOXING",
+  "TAEKWONDO",
+  "POWERLIFTING",
+  "CROSS FIT",
+  "PILATES",
+  "AEROBICS",
+  "CALISTHENICS",
+  "DANCE FITNESS",
+  "KARATE",
+  "SHOOTING"
 ];
 
 export const SPORTS_FACILITIES = [
-  "CRICKET", "FOOTBALL", "BASKETBALL", "BADMINTON", "TENNIS", "TABLE TENNIS", 
-  "VOLLEYBALL", "SQUASH", "PICKLEBALL"
+  "CRICKET",
+  "FOOTBALL",
+  "BASKETBALL",
+  "BADMINTON",
+  "TENNIS",
+  "TABLE TENNIS",
+  "VOLLEYBALL",
+  "SQUASH",
+  "PICKLEBALL",
+  "FUTSAL",
+  "HOCKEY",
+  "RUGBY",
+  "KABADDI",
+  "THROWBALL",
+  "HANDBALL"
 ];
 
 export const HERO_SLIDES = [
